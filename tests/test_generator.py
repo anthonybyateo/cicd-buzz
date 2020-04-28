1296213cf73fdb8a100d61cc3d1647b14679e005
test <test@test.com>
2020-04-28 15:16:30 +0300

    delete tests

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -1,12 +1,5 @@
 import unittest
 
-from buzz import generator
-
 def test_sample_single_word():
     assert 1 >= 0
 
-def test_sample_multiple_words():
-    assert 1 >= 0
-
-def test_generate_buzz_of_at_least_five_words():
-    assert 1 >= 0
