adbc9d0407dcacb50ab5be7a0f0148c332080d54
test <test@test.com>
2020-04-28 18:07:43 +0300

    1

--- .travis/deploy_heroku.sh
+++ .travis/deploy_heroku.sh
@@ -4,4 +4,4 @@ heroku plugins:install heroku-container-registry
 heroku plugins:install @heroku-cli/heroku-container-registry
 docker login -u _ --password=$HEROKU_API_KEY registry.heroku.com
 heroku container:push web --app $HEROKU_APP_NAME
-heroku container:release web --app $HEROKU_APP_NAME
+#heroku container:release web --app $HEROKU_APP_NAME
