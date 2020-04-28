298bcfe8b981ee3e25376e167d30869a3d6ad3b6
test <test@test.com>
2020-04-28 18:27:34 +0300

    4

--- app.py
+++ app.py
@@ -1,18 +1,21 @@
+
+"""Create python flask wrapper around our buzz generator"""
 import os
-import signal
 from flask import Flask
 from buzz import generator
 
+
 app = Flask(__name__)
 
-signal.signal(signal.SIGINT, lambda s, f: os._exit(0))
 
 @app.route("/")
 def generate_buzz():
+    """generate flask wrapper"""
     page = '<html><body><h1>'
     page += generator.generate_buzz()
     page += '</h1></body></html>'
     return page
 
+
 if __name__ == "__main__":
-    app.run(port=3000, host='212.98.171.122') # port 3000
+    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
